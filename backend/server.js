399493d1db945b2dacc7d659599f21e1b877a1c6
chenshuanlin <146963315+chenshuanlin@users.noreply.github.com>
2025-06-17 16:46:05 +0800

    sever.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const pool = require('./db');
 const serviceRoutes = require('./routes/service');
 const syncRoutes = require('./routes/syncRoutes');
 const statusRoutes = require('./routes/statusRoutes');
+const projectRoutes = require('./routes/projectRoutes');
 
 const app = express();
 
@@ -18,6 +19,7 @@ app.use(express.json()); // 解析 JSON 格式的請求體
 app.use('/customers', serviceRoutes);
 app.use('/sync-sheet-data', syncRoutes);
 app.use('/update-status', statusRoutes);
+app.use('/api/project', projectRoutes);
 
 // 啟動伺服器
 const PORT = process.env.PORT || 5713; // 從 .env 讀取埠號，如果沒有則預設 5713
